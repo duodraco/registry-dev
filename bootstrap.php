d06832e2188465ec12a219391b61e4598ea49ee9
Anderson Casimiro <o@duodra.co>
2013-06-05 02:00:22 -0300

    first scratches ...

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/* Include Path*/
+set_include_path(
+    get_include_path().PATH_SEPARATOR.
+    __DIR__.'/library'.PATH_SEPARATOR.
+    __DIR__.'/tests'
+    );
+/*Autolad*/
+spl_autoload_register(function ($classname) {
+    $classname = ltrim($classname, '\\');
+    preg_match('/^(.+)?([^\\\\]+)$/U', $classname, $match);
+    $classname = str_replace('\\', '/', $match[1]).
+        str_replace(['\\', '_'], '/', $match[2]).'.php';
+    require $classname;
+});
---
+++ tests/Leviathan/Registry/BaseTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Leviathan\Registry;
+
+class BaseTest extends \PHPUnit_Framework_TestCase
+{
+    protected $test = [
+        'a' => 'test 1',
+        'b' => 'test 2',
+        'c' => [
+            'new' => [
+                'namespace' => 'Leviathan'
+            ]
+        ]
+    ];
+    protected $expected = [
+        'a' => 'test 1',
+        'b' => 'test 2',
+        'c:new:namespace' => 'Leviathan'
+    ];
+
+    public function setUp()
+    {
+        $this->object = new Base();
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testFlatten()
+    {
+        $this->assertEquals(
+            $this->expected,
+            $this->object->flatten($this->test)
+        );
+    }
+    public function testStore()
+    {
+	$storeResult = $this->object->store('name','Registry');
+	$this->assertEquals(true,$storeResult);
+    }
+    public function testRetrieve()
+    {
+        $this->object->fill($this->test);
+        $this->assertEquals('Leviathan',$this->object->retrieve('c:new:namespace'));
+    }
+}
